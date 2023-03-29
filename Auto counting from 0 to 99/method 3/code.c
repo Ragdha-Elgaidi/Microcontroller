8e8449279e154d359bbac7fa3602bcc57e53fad0
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-03-29 02:33:36 +0200

    Update code.c

--- Auto counting from 0 to 99/method 3/code.c
+++ Auto counting from 0 to 99/method 3/code.c
@@ -6,14 +6,25 @@ void main()
 {
 char cnt1=0,cnt2=0,x;
 
-trisd=1;trisb=0;
-tens=0;ones=0;
+trisd=1;
+trisb=0;
+
+tens=0;
+ones=0;
 portb=0;
 
 while(count_switch==0);
 
 loop:
 cnt1++;
+
+if(cnt1==10)
+{
+cnt1=0;
+cnt2++;
+if(cnt2==10)
+  cnt2=0;
+}
 
 for(x=0;x<50;x++)
 {
