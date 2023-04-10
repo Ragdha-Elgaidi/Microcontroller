76ac4596603fb6c6f66f77971e1445ca501c684d
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-04-10 20:58:05 +0200

    Update code.c

--- Traffic light with flasher in night 12 AM/code.c
+++ Traffic light with flasher in night 12 AM/code.c
@@ -1,1 +1,45 @@
+void interrupt()
+{
+intcon.intf=0;
 
+while(1)
+{
+portc=0b00010010;
+delay_ms(1000);
+
+portc=0 ;
+delay_ms(1000);
+}
+
+}
+
+void main ()
+{
+trisc=0;
+trisd=1; 
+trisb=1;
+
+portc=0;
+
+intcon.inte=1;
+intcon.gie=1;
+option_reg.intedg=1;
+
+while (portd.f0==0); //check for start switch
+
+for (; ;)
+{
+portc=0b00100001;
+delay_ms(3000);
+ 
+portc=0b00110011;
+delay_ms(1000);
+ 
+portc=0b00001100;
+delay_ms(3000);
+ 
+portc=0b00011110; 
+delay_ms(1000);
+}
+
+}
